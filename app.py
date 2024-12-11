2f447164da7f9f7e390b9dc7f5971aad164cc7b4
LeeviY <ixus130@gmail.com>
2024-12-12 01:09:33 +0200

    Squashed commit of the following:
    commit ad332abcca2e7573ec3fb556c93d4ab7ee0b682b
    Author: LeeviY <ixus130@gmail.com>
    Date:   Mon Dec 9 05:03:55 2024 +0200
    
        fix extra dir path
    
    commit 60edec66fb73da3a0819e4150ecfe40c4adc9651
    Author: LeeviY <ixus130@gmail.com>
    Date:   Mon Dec 9 05:00:24 2024 +0200
    
        improved recalc
    
    commit 3936cce07bf9123b72c22e29a0707f6eb9b2b895
    Author: ossiyot <lumitulva@gmail.com>
    Date:   Sun Dec 8 18:17:38 2024 +0200
    
        Add december slippi games
    
    commit 42ed9c13511e18f02d2f1a79648aa7e7e85ef291
    Author: LeeviY <ixus130@gmail.com>
    Date:   Sun Dec 8 14:12:37 2024 +0200
    
        add option to toggle allowing exits
    
    commit 66005ea1cae1f7679bf3992aedd87cfe8281be99
    Author: LeeviY <ixus130@gmail.com>
    Date:   Sun Dec 8 13:59:08 2024 +0200
    
        Improved code quality
    
    commit 864a3b23961d3a16cfae873098aff0c12858b9ed
    Author: LeeviY <ixus130@gmail.com>
    Date:   Sun Dec 8 13:19:34 2024 +0200
    
        separated page files
    
    commit 51acf0499eaf32bbd0f4ddf172ad72d53cb91cc5
    Author: LeeviY <ixus130@gmail.com>
    Date:   Sun Dec 8 12:04:35 2024 +0200
    
        check if replay is not in progress
    
    commit 141ec9e9c5143f16d5d313ae0a5449df9286d2da
    Author: LeeviY <ixus130@gmail.com>
    Date:   Sun Dec 8 03:02:49 2024 +0200
    
        changed slippi parse library

--- app.py
+++ app.py
@@ -1,49 +1,107 @@
 import os
 import json
-import eventlet
 import re
+import eventlet
+import sys
+import contextlib
+
 
 eventlet.monkey_patch()
 
 from datetime import datetime
-from typing import List, Tuple
-from slippi import Game, id
+from typing import List, Tuple, Dict, Union
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 
+import win32file
+import peppi_py
+from slippi import Game, id
+from peppi_py import read_slippi, read_peppi
+
 
 app = Flask(__name__)
+app.json.sort_keys = False
 socketio = SocketIO(app)
 
+TIER_FILE = "tier_list.json"
+TIER_FILE_BASE = "tier_list.json.base"
+MIN_GAME_DURATION_SECONDS = 30
+ALLOW_EXIT = False
 
-tiers = {}
-with open("tier_list.json", "r") as file:
-    tiers = json.load(file)
+character_ratings = {}
+with open(TIER_FILE, "r") as file:
+    character_ratings = json.load(file)
 
-player_ports = {"P1": 2, "P2": 3}
+player_ports = {"P1": 2, "P2": 4}
+players_codes = {"P1": "LY＃863", "P2": "KEKW＃849"}
+
+EXTRA_DIRS = [f"{os.path.dirname(os.path.abspath(__file__))}\\2024-12"]
 
 
 ### Routing
 @app.route("/")
 def tier_list():
-    global tiers
-    return render_template("index.html", tiers=elo_to_tiers(tiers))
+    global character_ratings
+    return render_template("index.html", tiers=elo_to_tiers(character_ratings))
 
 
 @app.route("/reset", methods=["POST"])
 def reset_tier_list():
-    global tiers
-    with open("tier_list.json.base", "r") as file:
-        tiers = json.load(file)
+    global character_ratings
+    with open(TIER_FILE_BASE, "r") as file:
+        character_ratings = json.load(file)
 
-    with open("tier_list.json", "w") as file:
-        json.dump(tiers, file)
+    with open(TIER_FILE, "w") as file:
+        json.dump(character_ratings, file)
+
+    return jsonify(elo_to_tiers(character_ratings)), 200
+
+
+@app.route("/recalculate", methods=["POST"])
+def recalculate_tier_list():
+    global character_ratings, slippi_directory
+    with open(TIER_FILE_BASE, "r") as file:
+        character_ratings = json.load(file)
+
+    date_pattern = re.compile(r"^\d{4}-(0[1-9]|1[0-2])$")
+
+    replay_dirs = [
+        os.path.join(slippi_directory, month)
+        for month in os.listdir(slippi_directory)
+        if os.path.isdir(os.path.join(slippi_directory, month))
+        and date_pattern.match(month)
+    ]
+
+    replay_dirs += EXTRA_DIRS
+
+    for dir in replay_dirs:
+        for file in os.listdir(dir):
+            try:
+                path = os.path.join(dir, file)
+                process_replay(path)
+            except Exception as e:
+                print(f"Failed to parse file {file} in recalculation: {e}")
+
+    count_1 = 0
+    for character in character_ratings["P1"]:
+        count_1 += character["matches"]
+
+    count_2 = 0
+    for character in character_ratings["P2"]:
+        count_2 += character["matches"]
+
+    print(count_1, count_2)
+
+    with open(TIER_FILE, "w") as file:
+        json.dump(character_ratings, file)
+
+    with app.app_context():
+        return jsonify(elo_to_tiers(character_ratings)), 200
 
 
 @app.route("/port", methods=["GET"])
 def get_port():
     global player_ports
-    print(player_ports)
     return jsonify(player_ports), 200
 
 
@@ -65,67 +123,89 @@ def set_port():
         return jsonify({"error": "Player not found"}), 404
 
 
+@app.route("/allow_exit", methods=["POST"])
+def set_qutting():
+    global ALLOW_EXIT
+    data = request.json
+    ALLOW_EXIT = data.get("value")
+    return jsonify({"message": f"Allowing exit set to {ALLOW_EXIT}"}), 200
+
+
 @socketio.on("connect")
 def handle_connect():
-    emit("tier_update", elo_to_tiers(tiers))
+    emit("tier_update", elo_to_tiers(character_ratings))
 
 
 ### Logic
-def update_tiers(p1: Tuple[id.CSSCharacter, int], p2: Tuple[id.CSSCharacter, int]):
-    global tiers, socketio
-    p1_character, p1_stocks = p1
-    p2_character, p2_stocks = p2
-    p1_rating = tiers["P1"][p1_character]
-    p2_rating = tiers["P2"][p2_character]
-    p1_expected = 1.0 / (1.0 + pow(10, ((p1_rating - p2_rating) / 400)))
-    p2_expected = 1.0 / (1.0 + pow(10, ((p2_rating - p1_rating) / 400)))
+def update_tiers(
+    tiers: Dict[str, List[Dict[str, int]]],
+    p1: Dict[id.CSSCharacter, bool],
+    p2: Dict[id.CSSCharacter, bool],
+) -> Dict[str, List[Dict[str, int]]]:
+    if not p1 or not p2:
+        return None
 
-    if p1_stocks - p2_stocks == 0:
-        return
+    p1_char = tiers["P1"][p1["character"]]
+    p2_char = tiers["P2"][p2["character"]]
+    p1_rating = p1_char["elo"]
+    p2_rating = p2_char["elo"]
 
-    tiers["P1"][p1_character] = p1_rating + 100 * (
-        int(p1_stocks - p2_stocks > 0) - p1_expected
+    E_p = lambda R_a, R_b: 1.0 / (1.0 + pow(10, ((R_b - R_a) / 400)))
+    R_n = lambda R, K, S, E: R + K * (S - E)
+    K = lambda m: max(800 / (m + 1), 100)
+
+    p1_char["elo"] = R_n(
+        p1_rating,
+        K(p1_char["matches"]),
+        int(p1["won"]),
+        E_p(p1_rating, p2_rating),
     )
-    tiers["P2"][p2_character] = p2_rating + 100 * (
-        int(p2_stocks - p1_stocks > 0) - p2_expected
+    p2_char["elo"] = R_n(
+        p2_rating,
+        K(p2_char["matches"]),
+        int(p2["won"]),
+        E_p(p2_rating, p1_rating),
     )
+    p1_char["matches"] += 1
+    p2_char["matches"] += 1
 
-    with open("tier_list.json", "w") as file:
-        json.dump(tiers, file)
-
-    print(tiers)
-    socketio.emit("tier_update", elo_to_tiers(tiers))
+    return tiers
 
 
-def elo_to_tiers(tiers):
+def elo_to_tiers(
+    tiers: Dict[str, List[Dict[str, int]]]
+) -> Dict[str, List[Dict[str, Union[str, int]]]]:
     tier_list = {}
     for player, characters in tiers.items():
         tier_list[player] = {"S": [], "A": [], "B": [], "C": [], "D": [], "F": []}
-        for i, rating in enumerate(characters):
+        for i, character in enumerate(characters):
+            rating = character["elo"]
             tier = ""
-            if rating >= 2500:
+            if rating >= 2000:
                 tier = "S"
-            elif rating >= 2000:
+            elif rating >= 1800:
                 tier = "A"
-            elif rating >= 1500:
+            elif rating >= 1600:
                 tier = "B"
-            elif rating >= 1000:
+            elif rating >= 1400:
                 tier = "C"
-            elif rating >= 500:
+            elif rating >= 1200:
                 tier = "D"
             else:
                 tier = "F"
 
             tier_list[player][tier].append(
-                {"name": id.CSSCharacter(i).name, "rating": rating}
+                {
+                    "name": id.CSSCharacter(i).name,
+                    "rating": rating,
+                    "matches": character["matches"],
+                }
             )
 
-    print(tier_list)
     return tier_list
 
 
-def find_replay_directory():
-    # return "C:\\Users\\Lahela\\Documents\\Slippi\\test"
+def find_replay_directory() -> str:
     base_path = "C:\\Users"
     user_dirs = [
         os.path.join(base_path, user)
@@ -137,9 +217,12 @@ def find_replay_directory():
     latest_date = None
     latest_dir = None
 
+    slippi_dir = ""
+
     for user_dir in user_dirs:
         slippi_path = os.path.join(user_dir, "Documents", "Slippi")
         if os.path.exists(slippi_path) and os.path.isdir(slippi_path):
+            slippi_dir = slippi_path
             for subdir in os.listdir(slippi_path):
                 if date_pattern.match(subdir):
                     try:
@@ -155,14 +238,14 @@ def find_replay_directory():
     else:
         print("No valid directories found.")
 
-    return latest_dir
+    return slippi_dir, latest_dir
 
 
-slippi_directory = find_replay_directory()
-previous_files = set(os.listdir(slippi_directory))
+slippi_directory, latest_directory = find_replay_directory()
+previous_files = set(os.listdir(latest_directory))
 
 
-def detect_new_files(directory) -> str:
+def detect_new_files(directory: str) -> str:
     global previous_files
 
     current_files = set(os.listdir(directory))
@@ -176,49 +259,144 @@ def detect_new_files(directory) -> str:
     return ""
 
 
-def parse_replay(file_path: str) -> List[Tuple[id.CSSCharacter, int]]:
+def parse_replay(
+    file_path: str, debug_print: bool = False
+) -> Dict[int, Dict[str, Union[id.CSSCharacter, bool]]]:
     try:
-        game = Game(file_path)
+        game = read_slippi(file_path, skip_frames=True)
+        if debug_print:
+            print("Game: ", game)
+        if game.metadata["lastFrame"] / 60 < MIN_GAME_DURATION_SECONDS:
+            raise Exception("Game too short")
 
-        print(game)
-        if game.metadata.duration / 60 < 30:
-            print("Game too short")
-            return None
+        players = {}
+        for player in game.start.players:
+            if player.type != 0:
+                raise Exception("Non human player")
 
-        chars = [p.character if p else None for p in game.start.players]
+            players[player.port.value] = {
+                "code": player.netplay.code,
+                "character": id.CSSCharacter(player.character),
+            }
+
         # If zelda or sheik get the character from the last frame.
-        for i, p in enumerate(chars):
-            if p == id.CSSCharacter.ZELDA or p == id.CSSCharacter.SHEIK:
-                chars[i] = id.CSSCharacter[
-                    game.frames[-1].ports[i].leader.post.character.name
+        for port, player in players.items():
+            if (
+                player["character"] == id.CSSCharacter.ZELDA
+                or player == id.CSSCharacter.SHEIK
+            ):
+                chars = game.metadata["players"][str(port)]["characters"]
+                most_played = ("", 0)
+                for c, frames in chars.items():
+                    if frames > most_played[1]:
+                        most_played = (c, frames)
+                player["character"] = id.CSSCharacter[
+                    id.InGameCharacter(int(most_played[0])).name
                 ]
 
-        stocks = [100] * 4
-        for frame in game.frames:
-            for i, port in enumerate(frame.ports):
-                if port:
-                    stocks[i] = min(stocks[i], port.leader.post.stocks)
+        if not ALLOW_EXIT and game.end.method == 7:
+            raise Exception("Game exited")
 
-        return list(zip(chars, stocks))
+        # Quitter loses
+        for player in game.end.players:
+            if game.end.lras_initiator != None:
+                players[player.port]["won"] = player.port != game.end.lras_initiator
+            else:
+                players[player.port]["won"] = player.placement == 0
+
+        if debug_print:
+            print("Players:", players)
+        return players
 
     except Exception as e:
         print(f"An error occurred while parsing the file: {e}")
         return None
 
 
-def background_task():
-    print(f"Watching directory: {slippi_directory}")
+def is_file_locked(file_path: str) -> bool:
+    try:
+        handle = win32file.CreateFile(
+            file_path,
+            win32file.GENERIC_READ,
+            win32file.FILE_SHARE_READ,
+            None,
+            win32file.OPEN_EXISTING,
+            0,
+            None,
+        )
+        win32file.CloseHandle(handle)
+        return False
+    except Exception:
+        return True
+
+
+def process_replay(path: str, debug_print: bool = False) -> None:
+    global character_ratings
+    players = parse_replay(path, debug_print)
+    if not players:
+        return
+
+    player_codes = [x["code"] for x in players.values() if x["code"] != ""]
+
+    if (
+        (not players_codes["P1"] in player_codes)
+        or (not players_codes["P2"] in player_codes)
+    ) and len(player_codes) > 0:
+        return
+
+    get_player_port = lambda players, players_codes, player_key, default_port: next(
+        (
+            key
+            for key, value in players.items()
+            if value["code"] == players_codes[player_key]
+        ),
+        default_port - 1,
+    )
+
+    p1_port = get_player_port(players, players_codes, "P1", player_ports["P1"])
+    p2_port = get_player_port(players, players_codes, "P2", player_ports["P2"])
+
+    new_tiers = update_tiers(
+        character_ratings,
+        players.get(p1_port),
+        players.get(p2_port),
+    )
+
+    if new_tiers:
+        character_ratings = new_tiers
+
+
+def background_task() -> None:
+    print(f"Watching directory: {latest_directory}")
+    global character_ratings
+
+    socketio.emit("tier_update", elo_to_tiers(character_ratings))
+
     while True:
         new_file = detect_new_files(slippi_directory)
         if new_file != "":
             print(f"Found new replay: {new_file}")
-            result = parse_replay(os.path.join(slippi_directory, new_file))
-            if result:
-                update_tiers(
-                    result[player_ports["P1"] - 1], result[player_ports["P2"] - 1]
-                )
 
-        eventlet.sleep(0.1)
+            path = os.path.join(slippi_directory, new_file)
+            while is_file_locked(path):
+                eventlet.sleep(0.5)
+
+            process_replay(path, True)
+            with open(TIER_FILE, "w") as file:
+                json.dump(character_ratings, file)
+
+            socketio.emit("tier_update", elo_to_tiers(character_ratings))
+
+        eventlet.sleep(0.5)
+
+
+def recalculate_tier_list2(dir):
+    for file in os.listdir(dir):
+        try:
+            path = os.path.join(dir, file)
+            process_replay(path)
+        except Exception as e:
+            print(f"Failed to parse file {file} in recalculation: {e}")
 
 
 if __name__ == "__main__":
